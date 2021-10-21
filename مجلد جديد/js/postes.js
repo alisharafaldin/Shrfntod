6c6184677e6e1fa0988c8e1b0bb8b55944721ae7
alisharafaldinclear <alisharafaldin@gmail.com>
2021-10-21 16:58:21 +0300

    sh_22

---
+++ مجلد جديد/js/postes.js
@@ -0,0 +1,14 @@
+
+// Smooth scrolling
+$(".art-par a").on("click", function(event) {
+
+    if(this.hash !== "") {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $("html, body").animate({scrollTop: $(hash).offset().top - 100}, 800);
+    }
+  });
+  
+  
